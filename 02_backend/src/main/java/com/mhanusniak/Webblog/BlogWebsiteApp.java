2fce685f38ac17ad845d4081ffadb8ce249c32df
misohan <michal.hanusniak@aiesec.cz>
2021-05-10 22:23:46 +0200

    create skeleton of backend blog website application with Java Spring

---
+++ 02_backend/src/main/java/com/mhanusniak/Webblog/BlogWebsiteApp.java
@@ -0,0 +1,12 @@
+package com.mhanusniak.Webblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebsiteApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogWebsiteApp.class, args);
+    }
+}
---
+++ 02_backend/src/test/java/com/mhanusniak/Webblog/WebblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mhanusniak.Webblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
